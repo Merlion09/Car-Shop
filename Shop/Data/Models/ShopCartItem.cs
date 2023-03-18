92bf5810e19820dde4cbf131b07552886cb33cd9
Yuriy Khabal <khabal270@gmail.com>
2023-03-18 21:11:49 +0200

    Increased the maximum price of cars

--- Shop/Data/Models/ShopCartItem.cs
+++ Shop/Data/Models/ShopCartItem.cs
@@ -10,7 +10,7 @@ namespace Shop.Data.Models
         public int id { get; set; }
         public Car car { get; set; }
 
-        public int price { get; set; }
+        public uint price { get; set; }
 
         public string ShopCartId { get; set; }
     }
